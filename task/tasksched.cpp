9daded9e122cd2b373e8e7e0d3a4579b9e101dcb
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2021-05-08 03:39:49 +0200

    [TASKSCHED] Add support for tasks with time specification, and differentiate between them and "simple" timeless tasks.
    We support a structured text "task list file" with one task per line:
       time <whitespace> Task_description
    where:
    - time is in hour:minutes (HH:MM) format. This is optional.
    - Task_description is a one-line string describing the task.
    The tasks' timestamp is parsed with std::get_time().
    
    Internally, tasks are represented as CTask class objects, and are stored
    in two different queues, as they are treated in different orders:
    "timeless", i.e. "simple" enumerated tasks, versus timed tasks.
    --> One single queue wouldn't be sufficient.
    
    Timed tasks get sorted so as to be popped out in ascending order.
    --> This requires comparison operator(s) for CTasks to be implemented.
    
    NOTE: Since I try to stick to the original aim of the exercise, by using
    std::queue<>, I have to introduce that manual sorting function to sort
    the queue, and whose implementation is quite inefficient (to simplify it
    I move all the queued elements to an std::vector<> so as to be able to
    simply call std::sort() on it -- not possible for std:queue<> --, and
    then move the sorted elements back into the queue).
    It would have been better to use a more sophisticated queue-like
    structure instead, that supports sorting natively...
    
    For ease of usage, operator<< is overloaded for printing out task details
    to output streams.
    
    This code illustrates:
    - Structures / classes: https://youtu.be/iVLQeWbgbXs etc.
    - Templates: https://youtu.be/qrJjFN4Igfw
    
    and other concepts:
    - time_t, tm;
    - Overloading comparison and injection operators.

--- task/tasksched.cpp
+++ task/tasksched.cpp
@@ -8,6 +8,8 @@
  *              Based on the "Queue and FIFO/FCFS explained in 10 minutes"
  *              video challenge https://youtu.be/jaK4pn1jXTo by "CodeBeauty".
  *
+ * NOTE: Uses C++11 features.
+ *
  * Compilation:
  * - G++:   g++ tasksched.cpp -o tasksched.exe
  * - MSVC:  cl /EHsc tasksched.cpp /Fe:tasksched.exe
@@ -19,14 +21,35 @@
  *
  * A "task list file" could represent for example a daily schedule
  * (more practical when this schedule does not change much).
- * One task per line.
+ * One task per line:
+ *   time <whitespace> Task_description
+ * where:
+ * - 'time' is in hour:minutes (HH:MM) format. This is optional.
+ * - 'Task_description' is a one-line string describing the task.
+ *
+----------------------------
+6:00    Wake up
+6:30    Shower
+7:00    Breakfast
+7:30    Go to work
+8:00    Do work stuff
+12:00   Lunch break
+13:00   Back to work stuff
+14:30   Meeting
+16:00   Music lesson
  */
 
 /*
  * This program illustrates some of the concepts viewed in different videos,
  * using C++ and the STL library:
+ * - Structures / classes: https://youtu.be/iVLQeWbgbXs etc.
+ * - Templates: https://youtu.be/qrJjFN4Igfw
  * - Text file reading: https://youtu.be/EaHFhms_Shw
  * - STL queues: https://youtu.be/jaK4pn1jXTo
+ *
+ * Other concepts:
+ * - time_t, tm;
+ * - Overloading comparison and injection operators.
  */
 
 /* TEST MODE: Enable to compile and run this program in test mode. */
@@ -36,32 +59,222 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <ctime>        // For C++ time_t wrappers around "time.h".
 #include <iomanip>      // Used here for time parsing routines.
+/*
+ * HACK around braindead G++ -- Only usable for *nix!
+ * While the standard explicitly says that leading zeroes are optional
+ * in front of the digits in %H and %M formatting, G++ STL mandates them
+ * for std::get_time(), and fails parsing otherwise!
+ *
+ * See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=45896 (bug since 2010)
+ * and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=71556 (bug since 2016)
+ * for more details.
+ */
+#ifndef _WIN32
+#if defined(__GLIBCXX__) || defined(__GLIBCPP__)
+#define GCC_TIME_WORKAROUND
+#include <sys/time.h>
+#endif
+#endif
 //----
 #include <iostream>     // For IO streams.
 #include <fstream>      // For file streams.
+//----
 #include <queue>        // For std::queue<>
+#include <algorithm>    // For std::sort()
+//----
 #include <string>       // For std::string
-#ifdef TEST_MODE
 #include <sstream>      // For string streams.
-#endif
+//----
+#include <stdexcept>    // For std::runtime_error
 
 
 #ifdef TEST_MODE
 const std::string testSchedule =
 "  \t  00:00  \t  Midnight  \t  \n" // Leading/trailing whitespace (-> trim)
+"Generic_Task0\n"           // No time task, no whitespaces
 "\n\n"                      // Extra newlines (-> ignore)
 "   \t    \n"               // Only whitespace (-> ignore)
 "6:00  \t  Wake up\n"
+"6:30\n"                    // No description (-> ignore)
+"    Generic Task 1\n"      // No time task
 "7:00    Breakfast\n"
+"7:00    Bath\n"            // Colliding task
 "7:30    Go to work\n"
 "\n\n\n"                    // Extra newlines (-> ignore)
 "8:00    Do work stuff\n"
+"14:30   Meeting\n"         // Unordered item
 "12:00   Lunch break\n"
+"    Generic Task 2\n"      // No time task
+"16:00   Music lesson\n"    // Unordered item
 "13:00   Back to work stuff\n"
-"14:30   Meeting\n"
-"16:00   Music lesson\n"
 "\n\n";
 #endif
+
+/** @brief  Represents one task. */
+class CTask
+{
+public:
+
+/* Constructors */
+#if 0
+    CTask(const std::string timeStr,
+          const std::string& description) :
+    m_Time(timeStr),
+    m_Description(description)
+    {};
+#endif
+
+    CTask(const time_t time,
+          const std::string& description) :
+    m_Time(time),
+    m_Description(description)
+    {};
+
+    CTask(tm* const time,
+          const std::string& description) :
+    CTask(mktime(time), description)
+    {};
+
+    CTask(const std::string& description) :
+    CTask(time_t(-1), description)
+    {};
+
+/* Destructors */
+    virtual ~CTask() {}; // = default;
+
+/* Getters / Setters */
+    time_t time() const { return m_Time; }
+    void time(const time_t time) { m_Time = time; }
+    void time(tm* const time) { m_Time = mktime(time); }
+
+    const std::string& description() const { return m_Description; }
+    void description(const std::string& description) { m_Description = description; }
+
+/*
+ * Comparison operators - Used for comparing tasks (e.g. task queue sorting).
+ * NOTE: The C++20 fancy way is to define an auto operator<=>(const CTask&) const;
+ */
+    friend bool operator<(const CTask& task1, const CTask& task2);
+    friend bool operator>(const CTask& task1, const CTask& task2);
+    friend bool operator<=(const CTask& task1, const CTask& task2);
+    friend bool operator>=(const CTask& task1, const CTask& task2);
+    friend bool operator==(const CTask& task1, const CTask& task2) /*throw()*/;
+    friend bool operator!=(const CTask& task1, const CTask& task2) /*throw()*/;
+
+    /**
+     * @brief   Overloaded injection operator.
+     *          Used as a simple display function for the CTask.
+     *          Usage example:
+     *             cout << task;
+     *          Displays the time and description of the task.
+     */
+    friend std::ostream& operator<<(std::ostream& os, const CTask& task);
+
+protected:
+    time_t m_Time; // Timestamp. Is == -1 if no time associated.
+    std::string m_Description; // Task description.
+};
+
+/*
+ * Comparison operators
+ */
+
+/** @brief  Return true if task1.m_Time < task2.m_Time, and false otherwise. */
+bool operator<(const CTask& task1, const CTask& task2)
+{
+    /* Particular case for non-timed tasks */
+    if (task1.m_Time == -1)
+        return true;
+
+    /* General case */
+    return (task1.m_Time < task2.m_Time);
+}
+
+bool operator>(const CTask& task1, const CTask& task2)
+{
+    return (task2 < task1);
+}
+
+bool operator<=(const CTask& task1, const CTask& task2)
+{
+    return !(task1 > task2);
+}
+
+bool operator>=(const CTask& task1, const CTask& task2)
+{
+    return !(task1 < task2);
+}
+
+/**
+ * @brief   Return true if task1.m_Time == task2.m_Time, and false otherwise.
+ *          Throws an exception if the times are same but descriptions are
+ *          different (case of colliding tasks).
+ */
+bool operator==(const CTask& task1, const CTask& task2) /*throw()*/
+{
+    /* Return true if the objects are the same */
+    if (&task1 == &task2)
+        return true;
+    /* Do the actual comparison otherwise */
+    if (task1.m_Time == task2.m_Time)
+    {
+        if (task1.m_Description != task2.m_Description)
+            throw std::runtime_error("Colliding tasks!");
+        return true;
+    }
+    return false;
+}
+
+bool operator!=(const CTask& task1, const CTask& task2) /*throw()*/
+{
+    return !(task1 == task2);
+}
+
+
+/* Overloaded injection operator - Used for display. */
+std::ostream& operator<<(
+    std::ostream& os,
+    const CTask& task)
+{
+    tm* ptime = nullptr;
+
+    /* General case, except for non-timed tasks */
+    if (task.m_Time != -1)
+        ptime = localtime(&task.m_Time);
+
+    return (ptime ? (os << std::put_time(ptime, "%H:%M") << " -- ") : os)
+            << (!task.m_Description.empty() ? task.m_Description : "n/a");
+}
+
+
+/**
+ * @brief   Generic function for sorting std::queue<>'s in "non-descending
+ *          (i.e. ascending)" order, so that the elements are popped out in
+ *          increasing order.
+ *
+ *          NOTE: Implementation is not recursive.
+ *          This function is written because we don't use the more
+ *          sophisticated STL queue containers in this program...
+ */
+template <typename T>
+void sort(std::queue<T>& queue)
+{
+    std::vector<T> tmpArray;
+    while (!queue.empty())
+    {
+        tmpArray.push_back(queue.front());
+        queue.pop();
+    }
+    /* NOTE: The comparison predicate must induce a strict weak ordering
+     * relation; that's why we cannot use std::greater_equal<>, but need
+     * to use std::greater<> instead. */
+    std::sort(tmpArray.begin(), tmpArray.end(), std::greater<T>());
+    while (!tmpArray.empty())
+    {
+        queue.push(tmpArray.back());
+        tmpArray.pop_back();
+    }
+}
 
 
 // using std::cin;
@@ -120,15 +333,25 @@ int main(int argc, char** argv)
 
     /* Time support */
     time_t t_today = time(nullptr);
+    tm tm_today = *localtime(&t_today);
+    tm_today.tm_hour = tm_today.tm_min = tm_today.tm_sec = 0;
+    t_today = mktime(&tm_today);
 
     /*
      * Parse the tasks from the file.
      */
-    std::queue<std::string> tasks;
+    enum
+    {
+        SIMPLE_TASKS = 0,
+        TIMED_TASKS,
+        MAX_TYPE_TASKS
+    };
+    std::queue<CTask> tasks[MAX_TYPE_TASKS];
     while (true)
     {
         size_t pos;
         std::string line;
+        std::string timestr, description;
 
         /* Try to read an entire line representing a task */
         std::getline(inFile, line);
@@ -137,20 +360,56 @@ int main(int argc, char** argv)
         if (inFile.eof())
             break;
 
+        /*
+         * Parse the line: "HH:MM <whitespace> Task_description"
+         */
         constexpr const char* WHITESPACE = " \t\f\v\n\r";
 
-        /* Trim leading and trailing whitespace */
+        /* Trim leading whitespace */
         pos = line.find_first_not_of(WHITESPACE);
         line = line.substr(pos != std::string::npos ? pos : line.length());
-        pos = line.find_last_not_of(WHITESPACE);
-        line = line.substr(0, pos != std::string::npos ? pos + 1 : 0);
 
-        /* If the line is empty, skip this entry */
-        if (line.empty())
+        /* If we have a time string, it goes until the next whitespace */
+        pos = line.find_first_of(WHITESPACE);
+        timestr = line.substr(0, pos);
+
+        /* Try to parse and recognize the time string.
+         * NOTE: std::get_time() only modifies the necessary fields. */
+        tm tm_time = tm_today;
+#ifdef GCC_TIME_WORKAROUND
+        if (strptime(timestr.c_str(), "%H:%M", &tm_time) == nullptr)
+#else
+        if ((std::istringstream(timestr) >> std::get_time(&tm_time, "%H:%M")).fail())
+#endif
+        {
+            /* Parsing failed. The string is just the whole task description. */
+            timestr.clear();
+        }
+        else
+        {
+            /* Parsing succeeded */
+
+            /* Skip the time string */
+            line = line.substr(pos != std::string::npos ? pos : line.length());
+            /* Trim next whitespace */
+            pos = line.find_first_not_of(WHITESPACE);
+            line = line.substr(pos != std::string::npos ? pos : line.length());
+        }
+
+        /* Description string is everything else, without trailing whitespace */
+        pos = line.find_last_not_of(WHITESPACE);
+        description = line.substr(0, pos != std::string::npos ? pos + 1 : 0);
+
+        /* If no description, skip this entry */
+        if (description.empty())
             continue;
 
-        /* Append this new task */
-        tasks.push(line);
+        /* Append this new task to the correct queue */
+        // tasks.push(CTask(&tm_time, description));
+        if (timestr.empty())
+            tasks[SIMPLE_TASKS].emplace(description);
+        else
+            tasks[TIMED_TASKS].emplace(&tm_time, description);
     }
 
 #ifndef TEST_MODE
@@ -158,23 +417,44 @@ int main(int argc, char** argv)
     inFile.close();
 #endif
 
+    /* Sort the timed task queue in "ascending" order */
+    sort(tasks[TIMED_TASKS]);
+
+
     /* Print the header */
     cout << "==== Tasks for Today, "
          << std::put_time(localtime(&t_today), "%A %x")
          << " ====\n" << endl;
 
     /* Save whether we originally had tasks to do (used for later) */
-    bool bHadTasks = !tasks.empty();
+    bool bHadTasks = !tasks[SIMPLE_TASKS].empty() ||
+                     !tasks[TIMED_TASKS].empty();
 
-    /* Enumerate the tasks we need to do */
-    while (!tasks.empty())
+    /* First, enumerate any simple task we need to do */
+    if (!tasks[SIMPLE_TASKS].empty())
     {
-        /* Retrieve the next task and pop it */
-        cout << tasks.front() << endl;
-        tasks.pop();
-    }
-    if (bHadTasks)
+        cout << "To do:\n------\n" << endl;
+        while (!tasks[SIMPLE_TASKS].empty())
+        {
+            /* Retrieve the next task and pop it */
+            cout << tasks[SIMPLE_TASKS].front() << endl;
+            tasks[SIMPLE_TASKS].pop();
+        }
         cout << endl;
+    }
+
+    /* Then, run any scheduled timed task */
+    if (!tasks[TIMED_TASKS].empty())
+    {
+        cout << "Scheduled tasks:\n----------------\n" << endl;
+        while (!tasks[TIMED_TASKS].empty())
+        {
+            /* Retrieve the next task and pop it */
+            cout << tasks[TIMED_TASKS].front() << endl;
+            tasks[TIMED_TASKS].pop();
+        }
+        cout << endl;
+    }
 
     /* And we are done! */
     cout << (bHadTasks ? "You have finished all your tasks, congratulations! You've earned it!"
